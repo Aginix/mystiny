d2897f77236e139e1ef4f362eb3933fc7bbf9470
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2020-08-04 22:40:21 +0700

    Fix width of layout

--- packages/ui/src/components/FormLayout/FormLayoutItem.tsx
+++ packages/ui/src/components/FormLayout/FormLayoutItem.tsx
@@ -5,10 +5,10 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   item: {
-    flex: `1 1 22rem`,
+    flex: `1 1 10rem`,
     marginTop: theme.spacing(2),
-    marginLeft: theme.spacing(1.5),
-    marginRight: theme.spacing(1.5),
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
     maxWidth: `calc(100% - ${theme.spacing(2)}px)`,
   },
 }));
